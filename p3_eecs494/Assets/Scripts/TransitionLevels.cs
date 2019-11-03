944c5180adebb331dc16b11edc683e3524ac167e
Elisabeth Garber <elgarber@umich.edu>
2019-11-03 12:16:42 -0500

    completed level 2 layout

---
+++ p3_eecs494/Assets/Scripts/TransitionLevels.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionLevels : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Here");
+            
+            Scene scene = SceneManager.GetActiveScene();
+            Debug.Log(scene.name);
+            if(scene.name == "Level0")
+            {
+                SceneManager.LoadScene("Level1");
+            }
+            if(scene.name == "Level1")
+            {
+                SceneManager.LoadScene("Level2");
+            }
+            if(scene.name == "Level2")
+            {
+
+            }
+        }
+    }
+}
