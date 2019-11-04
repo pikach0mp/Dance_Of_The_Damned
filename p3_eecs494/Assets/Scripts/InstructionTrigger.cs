8c160ed3c21464f60c5147e1d800e6482215e86c
jessica <jeqyin@umich.edu>
2019-11-03 21:52:57 -0500

    added instruction trigger prefab and attack instruction

---
+++ p3_eecs494/Assets/Scripts/InstructionTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionTrigger : MonoBehaviour
+{
+    public float detection_distance = 2;
+    public GameObject instructions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * detection_distance, Color.green);
+        if (Physics.Raycast(transform.position, transform.forward, out hit, detection_distance))
+        {
+            if(hit.transform.gameObject.tag == "Player")
+            {
+                StartCoroutine(TimedPrompt());
+            }
+        }
+    }
+
+    IEnumerator TimedPrompt()
+    {
+        instructions.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        instructions.SetActive(false);
+    }
+}
