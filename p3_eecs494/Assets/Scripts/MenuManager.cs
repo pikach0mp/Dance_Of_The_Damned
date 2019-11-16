b455c202a561e5858f5b8bee270a780b769ad88a
jonnyliu <jonnyliu@umich.edu>
2019-11-16 09:22:07 -0500

    Added main menu, credits, and sync scene

---
+++ p3_eecs494/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void loadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void loadSync()
+    {
+        SceneManager.LoadScene("Sync");
+    }
+
+    public void loadCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void loadLevel()
+    {
+        SceneManager.LoadScene("FinalLayout");
+    }
+}
