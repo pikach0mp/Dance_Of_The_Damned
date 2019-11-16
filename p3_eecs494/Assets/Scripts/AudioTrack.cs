97d6daf1c7697fca6e40989f1cbd8d63393378bc
Adam Brown <adamcbrown1997@gmail.com>
2019-11-16 16:13:00 -0500

    Replaces music. Tracks now use AudioTrack scriptable objects

---
+++ p3_eecs494/Assets/Scripts/AudioTrack.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Audio/AudioTrack", order = 1)]
+public class AudioTrack : ScriptableObject
+{
+    public AudioClip audio;
+    public TextAsset beatData;
+
+    private (float, BeatInfo)[] beats;
+
+    public void OnValidate() {
+    	if(beatData == null) {
+    		beats = new (float, BeatInfo)[0];
+    		return;
+    	}
+    	string[] text = beatData.text.Split('\n');
+    	beats = new (float, BeatInfo)[text.Length-1];
+    	int i=0;
+    	foreach(string line in text) {
+    		if(line == "") {
+    			break;
+    		}
+    		string[] parts = line.Split(' ');
+    		BeatInfo info = new BeatInfo();
+    		info.noteInPattern = int.Parse(parts[1]);
+    		beats[i++] = (float.Parse(parts[0]), info);
+    	}
+
+    	int measureStartN=0, measureEndN=0;
+    	float measureStartT=0, measureEndT=0;
+    	for(i=0; i<beats.Length; ++i) {
+    		if(measureEndN == i) {
+    			measureStartN = measureEndN;
+    			measureStartT = beats[i].Item1;
+    			for (int j = i+1; j < beats.Length; ++j) {
+    				if(beats[j].Item2.noteInPattern == 1) {
+    					measureEndN = j;
+    					measureEndT = beats[measureEndN].Item1;
+    					break;
+    				}
+    			}
+
+    			if(measureStartN == measureEndN) {
+    				measureEndT = audio.length;
+    			}
+    		}
+
+    		BeatInfo info = beats[i].Item2;
+    		info.proportionalLocation = (beats[i].Item1 - measureStartT)/(measureEndT - measureStartT);
+    		beats[i].Item2 = info;
+
+    		Debug.Log(i+": "+beats[i].Item2.noteInPattern+", "+beats[i].Item2.proportionalLocation);
+    	}
+
+    	Debug.Assert(i == beats.Length);
+    }
+
+    public (float, BeatInfo) Get(int i) {
+    	if (i >= beats.Length || i < 0) {
+    		return (-1,new BeatInfo{});
+    	}
+    	return beats[i];
+    }
+
+    public int NumBeats() {
+    	return beats.Length;
+    }
+}
--- p3_eecs494/Assets/Scripts/BeatGenerator.cs
+++ p3_eecs494/Assets/Scripts/BeatGenerator.cs
@@ -10,30 +10,6 @@ public struct BeatInfo {
 }
 
 [System.SerializableAttribute]
-public class SongList {
-	public float[] pattern;
-
-	public float noteProportion(int note) {
-		float t = 0;
-		for(int i =0; i < pattern.Length; ++i) {
-			if(note == i) {
-				return t/getLength();
-			}
-			t += pattern[i];
-		}
-		return 1;
-	}
-
-	public float getLength() {
-		float len = 0;
-		foreach(float f in pattern) {
-			len += f;
-		}
-		return len;
-	}
-}
-
-[System.SerializableAttribute]
 public class UnityEventBeatInfo : UnityEvent<BeatInfo> {}
 [System.SerializableAttribute]
 public class UnityEventBeatInfoFloat : UnityEvent<(BeatInfo, float)> {}
@@ -47,11 +23,11 @@ public class BeatGenerator : MonoBehaviour {
 	public float offset;
 
 	private AudioSource source;
-    private AudioSource music;
+    // private AudioSource music;
 
     // Specifies time between beats
-    public SongList[] beatList;
-    public int level;
+    public AudioTrack track;
+	private int loops;
 
 	// Cache beats for lookAheadTime seconds
 	public double lookAheadTime;
@@ -78,42 +54,35 @@ public class BeatGenerator : MonoBehaviour {
         running = false;
         generateBeats = true;
 		times = new Queue<(BeatInfo, float)>();
-		source = GetComponents<AudioSource>()[0];
+		source = GetComponent<AudioSource>();
+		source.clip = track.audio;
 	}
 
 	void Update() {
-		// There was a loop
-		if(prevClipTime > source.time) {
-			float diff = source.clip.length % beatList[level].getLength();
-			if(diff > beatList[level].getLength()/2) {
-				diff -= beatList[level].getLength();
-			}
-			offset += diff;
-		}
-		prevClipTime = source.time;
 
 		if(!running) {
 			running = true;
 			startTime = AudioSettings.dspTime + 2;
-			nextPattern = 0;
 			lastTimeAdded = 0;
+			nextPattern = 0;
+			loops = 0;
 			source.PlayScheduled(startTime);
 		}
 
         while (lastTimeAdded < BeatGenerator.GetTime() + lookAheadTime) {
 
-			lastTimeAdded += beatList[level].pattern[nextPattern];
+			(float, BeatInfo) next = track.Get(nextPattern);
+			lastTimeAdded = next.Item1 + track.audio.length * loops;
 
-			BeatInfo info;
-			info.noteInPattern = nextPattern;
-			info.proportionalLocation = beatList[level].noteProportion(nextPattern);
 
 			if(generateBeats) {
-				times.Enqueue((info, lastTimeAdded));
-				onBeatAddedToQueue.Invoke((info, lastTimeAdded));
+				times.Enqueue((next.Item2, lastTimeAdded));
+				onBeatAddedToQueue.Invoke((next.Item2, lastTimeAdded));
 			}
+
 			nextPattern++;
-			if(nextPattern == beatList[level].pattern.Length) {
+			if(nextPattern == track.NumBeats()) {
+				loops++;
 				nextPattern = 0;
 			}
 		}
@@ -123,44 +92,44 @@ public class BeatGenerator : MonoBehaviour {
 		}
 	}
 
-	public static int GetLevel() {
-		return instance._GetLevel();
-	}
+	// public static int GetLevel() {
+	// 	return instance._GetLevel();
+	// }
 
-	public static bool SetLevel(int newLevel) {
-		return instance._SetLevel(newLevel);
-	}
+	// public static bool SetLevel(int newLevel) {
+	// 	return instance._SetLevel(newLevel);
+	// }
 
-	private int _GetLevel() {
-		return level;
-	}
+	// private int _GetLevel() {
+	// 	return level;
+	// }
 
 	public static void ToggleBeatSystem(bool play) {
 		instance.onPausePlay.Invoke(play);
 	}
 
-	private bool _SetLevel(int newLevel) {
-		if(newLevel < 0 || newLevel >= beatList.Length) {
-			return false;
-		}
+	// private bool _SetLevel(int newLevel) {
+	// 	if(newLevel < 0 || newLevel >= beatList.Length) {
+	// 		return false;
+	// 	}
 
-		level = newLevel;
+	// 	level = newLevel;
 
-		float time = lastTimeAdded % beatList[level].getLength();
-		float totalTime = 0;
+	// 	float time = lastTimeAdded % beatList[level].getLength();
+	// 	float totalTime = 0;
 
-		nextPattern = 0;
+	// 	nextPattern = 0;
 
-		for(int i = 0; i<beatList[level].pattern.Length; ++i) {			
-			if(totalTime >= time || Mathf.Abs(totalTime-time)< 1e-2) {
-				nextPattern = i;
-				break;
-			}
-			totalTime += beatList[level].pattern[i];
-		}
+	// 	for(int i = 0; i<beatList[level].pattern.Length; ++i) {			
+	// 		if(totalTime >= time || Mathf.Abs(totalTime-time)< 1e-2) {
+	// 			nextPattern = i;
+	// 			break;
+	// 		}
+	// 		totalTime += beatList[level].pattern[i];
+	// 	}
 
-		return true;
-	}
+	// 	return true;
+	// }
 
 	public void ToggleBeats(bool play) {
 		generateBeats = play;
