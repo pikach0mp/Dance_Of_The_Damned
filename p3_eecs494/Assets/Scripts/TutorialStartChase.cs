4f3eeab7bc314fae6582fe625c4352add547a296
Elisabeth Garber <elgarber@umich.edu>
2019-11-18 01:33:18 -0500

    Tutorial

---
+++ p3_eecs494/Assets/Scripts/TutorialStartChase.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialStartChase : MonoBehaviour
+{
+    public GameObject door;
+    public GameObject Camera;
+    public List<GameObject> chaseEnemies;
+    public float TimeB4Enemy = 1;
+
+    private bool first = true;
+
+    IEnumerator WaitForLookingAtDoor()
+    {
+        yield return new WaitForSeconds(4f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && first)
+        {
+            first = false;
+            // Disable Player Controls
+            // PlayerMovement movement = other.gameObject.GetComponent<PlayerMovement>();
+            // movement.OnDisable();
+            BeatGenerator.ToggleBeatSystem(false);
+
+            //Cut Scene
+            Camera.SetActive(true);
+
+            GetComponent<Animator>().SetTrigger("Level1Transition");
+            StartCoroutine(WaitForLookingAtDoor());
+
+            door.GetComponent<DoorController>().OpenDoor();
+            door.SetActive(true);
+
+            //nextDoor.GetComponent<DoorController>().OpenDoor();
+
+            //Re - Enable Player Controls
+            // movement.OnEnable();
+
+            //enable enemies
+            StartCoroutine(startEnemies());
+        }
+    }
+
+    IEnumerator startEnemies()
+    {
+        yield return new WaitForSeconds(TimeB4Enemy);
+        foreach (var enemy in chaseEnemies)
+        {
+            enemy.GetComponent<ChaseEnemy>().trigger(true);
+        }
+    }
+}
+
