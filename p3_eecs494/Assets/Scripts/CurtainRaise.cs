9a77a1e19617309f5515dba3df680f22da855ae8
Adam Brown <adamcbrown1997@gmail.com>
2019-12-08 16:37:07 -0500

    Changed curtains so it waits before raising

--- p3_eecs494/Assets/Scripts/CurtainRaise.cs
+++ p3_eecs494/Assets/Scripts/CurtainRaise.cs
@@ -6,13 +6,23 @@ public class CurtainRaise : MonoBehaviour {
 	public CurtainRaiseTrigger raiseTrigger;
 	private float yCutoff = 2.7F;
 
+	private float timeToStartRaising = 4F;
+	private float time;
+
 	void FixedUpdate() {
 		if(raiseTrigger.IsTriggered()) {
-			transform.position += Vector3.up * Time.fixedDeltaTime * 0.1F;
 
-			if(transform.position.y > yCutoff) {
-				gameObject.SetActive(false);
+			if(time < timeToStartRaising) {
+				time += Time.deltaTime;
+			} else {
+				transform.position += Vector3.up * Time.fixedDeltaTime * 0.1F;
+
+				if(transform.position.y > yCutoff) {
+					gameObject.SetActive(false);
+				}
 			}
+		} else {
+			time = 0;
 		}
 	}
 }
