0ae1842ba1f3f11e2e197e5732ca1106516f3693
Adam Brown <adamcbrown1997@gmail.com>
2019-12-08 00:50:07 -0500

    Added curtains raising while being it

---
+++ p3_eecs494/Assets/Scripts/CurtainRaise.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurtainRaise : MonoBehaviour {
+	public CurtainRaiseTrigger raiseTrigger;
+	private float yCutoff = 2.7F;
+
+	void FixedUpdate() {
+		if(raiseTrigger.IsTriggered()) {
+			transform.position += Vector3.up * Time.fixedDeltaTime * 0.1F;
+
+			if(transform.position.y > yCutoff) {
+				gameObject.SetActive(false);
+			}
+		}
+	}
+}
---
+++ p3_eecs494/Assets/Scripts/CurtainRaiseTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurtainRaiseTrigger : MonoBehaviour {
+
+	private bool triggered = false;
+
+	public bool IsTriggered() {
+		return triggered;
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		if(collider.GetComponent<PlayerMovement>() != null) {
+			triggered = true;
+		}
+	}
+
+	void OnTriggerExit(Collider collider) {
+		if(collider.GetComponent<PlayerMovement>() != null) {
+			triggered = false;
+		}
+	}
+}
